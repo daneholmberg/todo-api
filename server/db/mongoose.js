467806d9c78dd71ea8b4058e4fe2eaa48210fe55
Dane Holmberg <daneholmberg1@gmail.com>
2017-09-26 15:13:39 -0400

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var MongoDB = mongoose.connect('mongodb://localhost:27017/TodoApp').connection;
+MongoDB.on('error', function(err) { console.log(err.message); });
+MongoDB.once('open', function() {
+    console.log("mongodb connection open");
+});
 
 module.exports = {mongoose};
