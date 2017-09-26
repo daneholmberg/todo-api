d66bc8f18745480e088dc2588f4c12ebd205de73
Dane Holmberg <daneholmberg1@gmail.com>
2017-09-26 17:46:29 -0400

    Added DELETE /todos

--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ app.get('/todos/:id', (req, res) => {
     })
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.sendStatus(404);
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
