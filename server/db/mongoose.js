58305414e673de3d100ca4b7fdf6d461ad227fe7
Dane Holmberg <daneholmberg1@gmail.com>
2017-09-26 16:12:46 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var MongoDB = mongoose.connect('mongodb://localhost:27017/TodoApp').connection;
+var MongoDB = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp').connection;
+
 MongoDB.on('error', function(err) { console.log(err.message); });
 MongoDB.once('open', function() {
     console.log("mongodb connection open");
